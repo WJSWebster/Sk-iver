9dc1d9b7c7e725e1ccc417049b0ab81cf396fbbc
WJSWebster <ZBAC013@live.rhul.ac.uk>
2017-11-16 20:59:36 +0000

    installed and implemented SFML framework (and tested GitHub Slack intergration)

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 //
 // Created by William Webster on 15/11/2017.
+// SFML test
 //
-#include <iostream>
-#include "ScreenManager.h"
 
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main(){
-    //cout << "Hello, World!" << endl;
 
-    ScreenManager::getInstance().setText("Testing");
-    ScreenManager::getInstance().drawText();
+int main() {
 
-    cin.get(); // waits for input so not to immediately return & end
+    sf::RenderWindow window(sf::VideoMode(640,480,32),"Hello SFML");
+
+    sf::Font font;
+    font.loadFromFile("OpenSans-Bold.ttf");
+
+    sf::Text text("Hello World",font,11);
+    text.setCharacterSize(32);
+    text.setPosition(window.getSize().x/2 - text.getGlobalBounds().width/2,
+                     window.getSize().y/2 - text.getGlobalBounds().height/2);
+
+
+    while(window.isOpen()){
+
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+
+            window.clear(sf::Color::Black);
+            window.draw(text);
+            window.display();
+        }
+    }
     return 0;
 }
