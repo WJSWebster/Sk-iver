ca2b0cda043906f1009368753a3e4c6c3984e0ca
WJSWebster <ZBAC013@live.rhul.ac.uk>
2017-11-29 19:42:56 +0000

    implemented SFML 'Fonts and Text' function, via a title screen. However, noted that sf::Text cannot render 'modifier letters' (either greek(ᵧ) or latin(ᴰ) etc.)

--- main.cpp
+++ main.cpp
@@ -7,18 +7,16 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-#include <ctime>
-#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
     sf::Vector2i screenDimensions(800, 600);
-    sf::Vector2i blockDimensions(10, 10);
+    sf::Vector2i blockDimensions(10, 10); // (only used for random noise example)
 
     sf::RenderWindow window;
-    window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "My first SFML Game", sf::Style::Titlebar | sf::Style::Close); // | sf::Style::Resize);
+    window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "My first SFML Game", sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize);
 
     sf::Clock clock;
     sf::SoundBuffer soundBuffer; // TODO currently not used
@@ -30,22 +28,34 @@ int main()
     window.setSize(size);
 //    window.setTitle("Skᵧ ᴰiver");
     window.setPosition(sf::Vector2i(200, 100));
-    window.setKeyRepeatEnabled(false); // TODO investigate
+//    window.setKeyRepeatEnabled(false); // TODO investigate
     window.setVerticalSyncEnabled(1);
     window.setFramerateLimit(60);
 
-    srand(time(0)); // TODO investigate - is this generating a random seed based on time?
+    //srand(time(0)); // TODO investigate - is this generating a random seed based on time?
 
     float frameCounter = 0, switchFrame = 100, frameSpeed = 500;
 
     // loading the player sprite
     sf::Texture playerTexture;
-    if(!playerTexture.loadFromFile("placeholder_spritesheet.png")){
+    if(!playerTexture.loadFromFile("placeholder_spritesheet.png"))
         cout << "ERROR: Could not load player image." << endl;
-    }
 
     sf::Sprite playerImage;
     playerImage.setTexture(playerTexture);
+
+    sf::Font font;
+    if(!font.loadFromFile("Font/Arial_Unicode.ttf"))
+        cout << "ERROR: Could not load font file." << endl;
+
+//    basic_string<sf::Uint32> string = {0x53, 0x6B, 0x671D0000, 0x20, 0x1D30, 0x69, 0x76, 0x65, 0x72};
+//    sf::String sentence(string);
+//    sf::String sentence = "Skᵧ ᴰiver";
+
+    sf::Text text("Sk-iver", font, 60); // Title
+    text.setColor(sf::Color::Magenta); // function deprecated?
+    text.setStyle(sf::Text::Bold);
+    text.setPosition(300, 50); // TODO needs to be soft-coded, so relative to resolution etc
 
     enum spriteDirection { Down, Left, Right, Up };
     sf::Vector2i source(0, Down);
@@ -94,6 +104,7 @@ int main()
                     break;
 
                 case sf::Event::Resized:
+//                    window.setView(sf::View(sf::FloatRect(0, 0, Event.size.width, Event.size.height))); // letterboxes to keep same original resolution
                     cout << "New width: " << Event.size.width << ", new height: " << Event.size.height << endl;
                     break;
 
@@ -186,6 +197,7 @@ int main()
 
         window.draw(circle);
         window.draw(playerImage);
+        window.draw(text);
         window.display();
 
         window.clear();
